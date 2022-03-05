797006e9c02a37a1ec29b84e1fb47024c46ad4b9
Burki24 <burkhard@kneiseler.de>
2022-03-05 06:53:47 +0100

    Status für 3 Fenster

--- WindowState/module.php
+++ WindowState/module.php
@@ -9,16 +9,15 @@
 
             $this->RegisterPropertyInteger("Reed1ID", 0);
             $this->RegisterPropertyInteger("Reed2ID", 0);
+            $this->RegisterPropertyInteger("Reed3ID", 0);
             $this->RegisterPropertyInteger("WindowOpenMode", 0);
-            $this->RegisterPropertyInteger("WindowTiltMode", 0);
-
-            if (!IPS_VariableProfileExists("SIT.WindowState")){
-                IPS_CreateVariableProfile("SIT.WindowState", 1);
-                IPS_SetVariableProfileValues("SIT.WindowState", 0, 2, 0);
-                IPS_SetVariableProfileDigits("SIT.WindowState", 0);
-                IPS_SetVariableProfileAssociation("SIT.WindowState", 0, "geschlossen", "Window", -1);
-                IPS_SetVariableProfileAssociation("SIT.WindowState", 1, "gekippt", "Window", -1);
-                IPS_SetVariableProfileAssociation("SIT.WindowState", 2, "geöffnet", "Window", -1);
+            $Profile = {"WGS.WindowState"};
+            if (!IPS_VariableProfileExists($Profile)){
+                IPS_CreateVariableProfile($Profile, 1);
+                IPS_SetVariableProfileValues($Profile, 0, 2, 0);
+                IPS_SetVariableProfileDigits($Profile, 0);
+                IPS_SetVariableProfileAssociation($Profile, 0, "geschlossen", "Window", -1);
+                IPS_SetVariableProfileAssociation($Profile, 1, "geöffnet", "Window", -1);
             }
 
             $this->RegisterVariableInteger("WindowState", "WindowState", "SIT.WindowState", 1);
@@ -32,6 +31,7 @@
 
             $this->RegisterMessage($this->ReadPropertyInteger("Reed1ID"), 10603 /* VM_UPDATE */);
             $this->RegisterMessage($this->ReadPropertyInteger("Reed2ID"), 10603 /* VM_UPDATE */);
+            $this->RegisterMessage($this->ReadPropertyInteger("Reed3ID"), 10603 /* VM_UPDATE */);
 
         }
 
@@ -49,68 +49,58 @@
         private function UpdateWindowState() {
             $Reed1 = GetValue($this->ReadPropertyInteger("Reed1ID"));
             $Reed2 = GetValue($this->ReadPropertyInteger("Reed2ID"));
+            $Reed2 = GetValue($this->ReadPropertyInteger("Reed3ID"));
 
             $WindowOpenMode = $this->ReadPropertyInteger("WindowOpenMode");
-            $WindowTiltMode = $this->ReadPropertyInteger("WindowTiltMode");
-
+            
             $this->SendDebug("WindowState", "WindowOpenMode: " . $WindowOpenMode, 0);
-            $this->SendDebug("WindowState", "WindowTiltMode: " . $WindowTiltMode, 0);
             $this->SendDebug("WindowState", "Reed 1: " . (int)$Reed1, 0);
             $this->SendDebug("WindowState", "Reed 2: " . (int)$Reed2, 0);
+            $this->SendDebug("WindowState", "Reed 3: " . (int)$Reed3, 0);
 
             $WindowState = 0;
 
             switch ($WindowOpenMode){
                 case 0:
-                    if (!$Reed1 && !$Reed2){
+                    if (!$Reed1 && !$Reed2 && !$Reed3){
                         $WindowState = 2;
                     }
                 break;
 
                 case 1:
-                    if (!$Reed1 && $Reed2){
+                    if (!$Reed1 && !$Reed2 && $Reed3){
                         $WindowState = 2;
                     }
                 break;
 
                 case 2:
-                    if ($Reed1 && !$Reed2){
+                    if (!$Reed1 && $Reed2 && $Reed3){
                         $WindowState = 2;
                     }
                 break;
 
                 case 3:
-                    if ($Reed1 && $Reed2){
+                    if (!$Reed1 && $Reed2 && !$Reed3){
+                        $WindowState = 2;
+                    }
+                break;
+                case 4:
+                    if ($Reed1 && !$Reed2 && !$Reed3){
+                        $WindowState = 2;
+                    }
+                break;
+                case 5:
+                    if ($Reed1 && !$Reed2 && $Reed3){
+                        $WindowState = 2;
+                    }
+                break;
+                case 6:
+                    if ($Reed1 && $Reed2 && !$Reed3){
                         $WindowState = 2;
                     }
                 break;
             }
-
-            switch ($WindowTiltMode){
-                case 0:
-                    if (!$Reed1 && !$Reed2){
-                        $WindowState = 1;
-                    }
-                break;
-
-                case 1:
-                    if (!$Reed1 && $Reed2){
-                        $WindowState = 1;
-                    }
-                break;
-
-                case 2:
-                    if ($Reed1 && !$Reed2){
-                        $WindowState = 1;
-                    }
-                break;
-
-                case 3:
-                    if ($Reed1 && $Reed2){
-                        $WindowState = 1;
-                    }
-                break;
-            }
+            
             SetValue($this->GetIDForIdent("WindowState"), $WindowState);
             $this->SendDebug("WindowState", "Window State: " . (int)$WindowState, 0);
         }
