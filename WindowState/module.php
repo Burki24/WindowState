ca1544db5a67e2a1248ec44fa118e491b401e6f1
Burki24 <burkhard@kneiseler.de>
2022-03-06 06:50:21 +0100

    Reduziert auf 3 Reeds

--- WindowState/module.php
+++ WindowState/module.php
@@ -10,10 +10,6 @@
             $this->RegisterPropertyInteger("Reed1ID", 0);
             $this->RegisterPropertyInteger("Reed2ID", 0);
             $this->RegisterPropertyInteger("Reed3ID", 0);
-            $this->RegisterPropertyInteger("Reed4ID", 0);
-            $this->RegisterPropertyInteger("Reed5ID", 0);
-            $this->RegisterPropertyInteger("Reed6ID", 0);
-            
             $this->RegisterPropertyInteger("WindowOpenMode", 0);
             $Profile = "WGS.WindowGroup";
             if (!IPS_VariableProfileExists($Profile)){
@@ -36,10 +32,6 @@
             $this->RegisterMessage($this->ReadPropertyInteger("Reed1ID"), 10603 /* VM_UPDATE */);
             $this->RegisterMessage($this->ReadPropertyInteger("Reed2ID"), 10603 /* VM_UPDATE */);
             $this->RegisterMessage($this->ReadPropertyInteger("Reed3ID"), 10603 /* VM_UPDATE */);
-            $this->RegisterMessage($this->ReadPropertyInteger("Reed4ID"), 10603 /* VM_UPDATE */);
-            $this->RegisterMessage($this->ReadPropertyInteger("Reed5ID"), 10603 /* VM_UPDATE */);
-            $this->RegisterMessage($this->ReadPropertyInteger("Reed6ID"), 10603 /* VM_UPDATE */);
-
         }
 
         public function MessageSink($TimeStamp, $SenderID, $Message, $Data) {
@@ -57,20 +49,14 @@
             $Reed1 = GetValue($this->ReadPropertyInteger("Reed1ID"));
             $Reed2 = GetValue($this->ReadPropertyInteger("Reed2ID"));
             $Reed3 = GetValue($this->ReadPropertyInteger("Reed3ID"));
-            $Reed4 = GetValue($this->ReadPropertyInteger("Reed4ID"));
-            $Reed5 = GetValue($this->ReadPropertyInteger("Reed5ID"));
-            $Reed6 = GetValue($this->ReadPropertyInteger("Reed6ID"));
             $WindowOpenMode = $this->ReadPropertyInteger("WindowOpenMode");
             
             $this->SendDebug("WindowGroup", "WindowOpenMode: " . $WindowOpenMode, 0);
             $this->SendDebug("WindowGroup", "Reed 1: " . (int)$Reed1, 0);
             $this->SendDebug("WindowGroup", "Reed 2: " . (int)$Reed2, 0);
             $this->SendDebug("WindowGroup", "Reed 3: " . (int)$Reed3, 0);
-            $this->SendDebug("WindowGroup", "Reed 4: " . (int)$Reed4, 0);
-            $this->SendDebug("WindowGroup", "Reed 5: " . (int)$Reed5, 0);
-            $this->SendDebug("WindowGroup", "Reed 6: " . (int)$Reed6, 0);
             $WindowState = 0;
-            if (($Reed1 != 1) || ($Reed2 != 1) || ($Reed3 != 1) || ($Reed4 != 1) || ($Reed5 != 1) || ($Reed6 != 1)){
+            if (($Reed1 != 1) || ($Reed2 != 1) || ($Reed3 != 1)){
                 $WindowGroup = 1;
             }
             SetValue($this->GetIDForIdent("WindowGroup"), $WindowGroup);
